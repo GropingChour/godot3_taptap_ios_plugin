e12df0bbc6096d983e2d0e35f2448618ebe26d4f
Ahogeyi <gropingchour@qq.com>
2025-12-15 10:21:37 +0800

    fix: fix sh for only taptap plugin

--- scripts/release_xcframework.sh
+++ scripts/release_xcframework.sh
@@ -3,9 +3,9 @@ set -e
 
 if [[ "$1" == "3.x" ]];
 then
-    GODOT_PLUGINS="gamecenter inappstore icloud camera arkit apn photo_picker"
+    GODOT_PLUGINS="godot3taptap"
 else
-    GODOT_PLUGINS="gamecenter inappstore icloud camera apn photo_picker"
+    GODOT_PLUGINS="godot3taptap"
 fi
 
 # Compile Plugin
