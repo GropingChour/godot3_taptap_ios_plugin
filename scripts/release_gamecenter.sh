403dca5adedc0867f74839520e5459cfb6750084
Ahogeyi <gropingchour@qq.com>
2026-04-01 10:27:46 +0800

    feat: add gamecenter build

---
+++ scripts/release_gamecenter.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+if [[ "$1" == "3.x" ]];
+then
+    GODOT_PLUGINS="gamecenter"
+else
+    GODOT_PLUGINS="gamecenter"
+fi
+
+# Compile Plugin
+for lib in $GODOT_PLUGINS; do
+    ./scripts/generate_xcframework.sh $lib release $1
+    ./scripts/generate_xcframework.sh $lib release_debug $1
+    mv ./bin/${lib}.release_debug.xcframework ./bin/${lib}.debug.xcframework
+done
+
+# Ensure release folder exists
+if [ ! -d "./bin/release" ]; then
+    mkdir ./bin/release
+fi
+
+# Move Plugin
+for lib in $GODOT_PLUGINS; do
+    if [ -d "./bin/release/${lib}" ]; then
+        rm -rf ./bin/release/${lib}
+    fi
+    mkdir ./bin/release/${lib}
+    mv ./bin/${lib}.{release,debug}.xcframework ./bin/release/${lib}
+    cp ./plugins/${lib}/${lib}.gdip ./bin/release/${lib}
+done
